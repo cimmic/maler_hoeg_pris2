30b455c3c4f1c58f640ce50cc52a10f872fcabd0
Kim Høg <cimmik@yahoo.dk>
2016-05-30 10:00:19 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var inputbox, paragraph;
+var validInputs = "0123456789,.";//If a char that isn't in this string is inputted, the char will not be displayed.
+var filteredInput;
+var temp;
+var prisFor, prisBelob;
+
+function setup() {
+  
+}
+
+function draw() {
+  
+}
